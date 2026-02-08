990bd943f9b37c3b84a11d0f7e0017c3f8f2f9c1
Devidas-G <Dev.gaikwad222@gmail.com>
2026-02-08 16:03:23 +0530

    update router

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,12 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/Home'
-import About from './pages/about'
-
-function AppRouter() {
-  const [route, setRoute] = useState<string>(
-    // strip leading '#' if present
-    (window.location.hash || '').replace(/^#/, ''),
-  )
-
-  useEffect(() => {
-    const onHash = () => setRoute((window.location.hash || '').replace(/^#/, ''))
-    window.addEventListener('hashchange', onHash)
-    return () => window.removeEventListener('hashchange', onHash)
-  }, [])
-
-  if (route === 'about') return <About />
-  // default to home for unknown or empty route
-  return <Home />
-}
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
